c91bfa519873d7a8b43bd0bc6ad1a3a64c1bff5f
kashfi-khan <86517455+kashfi-khan@users.noreply.github.com>
2022-04-18 19:23:35 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text, TextInput, Image, ScrollView, Button, TouchableOpacity, FlatList } from 'react-native'
+import Navigation from './android/app/src/config/Navigation'
+
+function App() {
+  return (
+
+    
+      <Navigation/>
+
+
+    
+  )
+}
+
+export default App
